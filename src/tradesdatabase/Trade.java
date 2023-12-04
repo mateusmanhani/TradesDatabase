36693fa661443b0fc055aed57b9eb6587d3900c0
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2023-12-04 13:20:19 +0000

    Create constructors

--- src/tradesdatabase/Trade.java
+++ src/tradesdatabase/Trade.java
@@ -1,9 +1,35 @@
 package tradesdatabase;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class Trade {
+    private String tradeOrigin;
+    private String tradeDestination;
+    private double tradePrice;
+    private int tradeAmount;
+    private String tradeType;
+    private final ArrayList<String> tradeInfo = new ArrayList<>();
     
+    // Default constrcutor
+    public Trade(){
+        // Initialize with default values or leave fields uninitialized
+        tradeOrigin = "";
+        tradeDestination = "";
+        tradePrice = 0.0;
+        tradeAmount = 0;
+        tradeType = "";
+    }
+    
+    //create constructor for a trade w/ origin,destination, price, amount and type
+    public Trade (String tradeOrigin, String tradeDestination,double tradePrice,int tradeAmount, String tradeType){
+        this.tradeOrigin = tradeOrigin;
+        this.tradeDestination = tradeDestination;
+        this.tradePrice = tradePrice;
+        this.tradeAmount = tradeAmount;
+        this.tradeType = tradeType;
+    }    
 }
