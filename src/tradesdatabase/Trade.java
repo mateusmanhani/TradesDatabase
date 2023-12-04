893aaba51e4a10cfc60363e0412ed2e4b9a6fd3d
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2023-12-04 13:20:59 +0000

    Getters

--- src/tradesdatabase/Trade.java
+++ src/tradesdatabase/Trade.java
@@ -31,5 +31,36 @@ public class Trade {
         this.tradePrice = tradePrice;
         this.tradeAmount = tradeAmount;
         this.tradeType = tradeType;
-    }    
+    }
+        // Getter for trade origin
+    public  String getTradeOrigin(){
+        return tradeOrigin;
+    }       
+    // Getter for trade destination
+    public String getTradeDestination(){
+        return tradeDestination;
+    }
+    // Getter for trade price
+    public double getTradePrice(){
+        return tradePrice;
+    }
+    // Getter for trade amount
+    public int getTradeAmount(){
+        return tradeAmount;
+    }
+    // Getter for trade type
+    public String getTradeType(){
+        return tradeType;
+    }
+    //Getter for whole trade info| returns an arrayList of attributes
+    public ArrayList<String> getTradeInfo(){
+        if(tradeInfo.isEmpty()) { 
+            tradeInfo.add(tradeOrigin);
+            tradeInfo.add(tradeDestination);
+            tradeInfo.add(String.valueOf(tradePrice));
+            tradeInfo.add(String.valueOf(tradeAmount));
+            tradeInfo.add(tradeType);
+        }
+        return tradeInfo;
+    }
 }
